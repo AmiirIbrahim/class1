fc6cb6e6e6d59ace5957b55175620804c537edd0
AmiirIbrahim <amiir.ibrahim@strathmore.edu>
2024-07-04 12:18:05 +0300

    created and modified services pages

--- services.php
+++ services.php
@@ -4,26 +4,29 @@
         <meta hhtp_equiv="Content-Type"content="text/html;
         charset=UTF-8">
         <title>Welcome - Document</title>
+        <link rel="stylesheet" href="services.css">
         <!DOCTYPE html>
 <html>
     <head>
         <meta hhtp_equiv="Content-Type"content="text/html;
         charset=UTF-8">
         <title>Welcome - Document</title>
-        <style>
-            body{
-                background-image: url("images/plane5.jpg");
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-attachment: fixed;
-                background-position: center;
-            }
+       
+           
 
-        </style>
-        <a href=",/">About</a>|<a href="fleet.html"> Fleet</a>|<a href="destination.html"> Destinations</a>|<a href="mobile.html"> Mobile App</a>|<a href="r.html">Sign up</a>|<a href="signin.html">Sign in</a>|<a href="booking.html">Book a flight</a>|<a href="services.html">Services</a><br><br>z
+           <nav class="nav">
+        <a href="index.php">About</a>
+        <a href="fleet.html"> Fleet</a>
+        <a href="destination.html"> Destinations</a>
+        <a href="mobile.html"> Mobile App</a>
+        <a href="r.html">Sign up</a>
+        <a href="signin.html">Sign in</a>
+        <a href="booking.html">Book a flight</a>
+        <a href="services.html">Services</a><br><br>
+           </nav>
 
-        <body>
-            <p style="text-align: left;font-weight: bold;color: black;" ;">
+        <body class="p1">
+            </p>
                 We make every effort to transport and provide seamless travel experience for all our guests, particularly those with special needs or disabilities.
 Such guests may request for assistance through our reservation representatives assistance while traveling, such as transportation to and from gates either by wheelchair or by electric cart (according to the airport facility), boarding assistance and dietary needs.
             
